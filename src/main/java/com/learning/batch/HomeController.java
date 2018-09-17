8d355c313705c3a8f1b439bcfc9cb3c4c552f2c3
Gaurav Agarwal <gauravbits2000@gmail.com>
2018-09-18 01:34:49 +0530

    Swagger Changes

--- src/main/java/com/learning/batch/HomeController.java
+++ src/main/java/com/learning/batch/HomeController.java
@@ -6,8 +6,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 //@RestController  // using @RestController annotation
 @Controller
+@Api(value="homeController-Helloo", description="Description about the Controller Method") 
+
+@ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Successfully retrieved list"),
+        @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+        @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+        @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+        })
 public class HomeController 
 {
 	@RequestMapping("/hello")  
